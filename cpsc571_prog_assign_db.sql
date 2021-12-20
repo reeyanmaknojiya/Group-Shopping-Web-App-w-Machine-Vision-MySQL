12e1e83564231496cffc829e637e90c0fe306646
reeyan.maknojiya <reeyan.maknojiya@gmail.com>
2021-12-19 23:18:32 -0700

    updated database

--- cpsc571_prog_assign_db.sql
+++ cpsc571_prog_assign_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 30, 2021 at 08:32 PM
+-- Generation Time: Dec 20, 2021 at 07:16 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.4.26
 
@@ -20,6 +20,50 @@ SET time_zone = "+00:00";
 --
 -- Database: `cpsc571_prog_assign_db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `groups`
+--
+
+CREATE TABLE `groups` (
+  `group_name` varchar(100) NOT NULL,
+  `group_id` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `groups`
+--
+
+INSERT INTO `groups` (`group_name`, `group_id`) VALUES
+('Clothing', 2),
+('Electronics', 3),
+('Grocery', 1),
+('Sneakers', 4),
+('Sports', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `group_memberships`
+--
+
+CREATE TABLE `group_memberships` (
+  `user_id` bigint(20) NOT NULL,
+  `group_id` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `group_memberships`
+--
+
+INSERT INTO `group_memberships` (`user_id`, `group_id`) VALUES
+(1, 1),
+(1, 2),
+(1, 4),
+(2, 3),
+(2, 1);
 
 -- --------------------------------------------------------
 
@@ -46,6 +90,21 @@ INSERT INTO `users` (`user_id`, `user_name`, `email`, `password`, `id`) VALUES
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `groups`
+--
+ALTER TABLE `groups`
+  ADD PRIMARY KEY (`group_id`),
+  ADD KEY `group_name` (`group_name`),
+  ADD KEY `group_id` (`group_id`);
+
+--
+-- Indexes for table `group_memberships`
+--
+ALTER TABLE `group_memberships`
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `group_id` (`group_id`);
 
 --
 -- Indexes for table `users`
